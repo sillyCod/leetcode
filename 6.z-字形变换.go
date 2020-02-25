a7242f1fbd8a88f31a437e0c95e0feb6d09dc9d0
wangfusheng <wangfusheng@chuangxin.com>
2020-02-25 19:55:18 +0800

    z transform.

---
+++ 6.z-字形变换.go
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=6 lang=golang
+ *
+ * [6] Z 字形变换
+ */
+
+// @lc code=start
+func convert(s string, numRows int) string {
+	return ""
+}
+func concat(source string) string {
+	var temp []uint
+	for _, v := range source {
+
+	}
+}
+// @lc code=end
+
